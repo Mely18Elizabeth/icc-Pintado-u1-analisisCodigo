5b2b40e4848e4154166f9996fb361da82b894b32
Melany <145609279+Mely18Elizabeth@users.noreply.github.com>
2025-04-30 08:47:28 -0500

    timePython

---
+++ src_python/app.py
@@ -0,0 +1,6 @@
+#import benchmarking as ben
+from benchmarking import Benchmarking
+
+if __name__ == "__main__":
+    print("Funciona")
+    Benchmarking()
---
+++ src_python/benchmarking.py
@@ -0,0 +1,35 @@
+import random
+import time
+from metodos_ordenamiento import MetodosOrdenamiento
+class Benchmarking:
+    
+    def __init__(self):
+        print('Bench inicializado')
+        self.mOrdenamiento= MetodosOrdenamiento()
+        arreglo= self.build_arreglo(1000)
+
+        tarea= lambda: self.mOrdenamiento.sortByBubble(arreglo)
+        tiempoMillis = self.contar_con_current_time_milles(tarea)
+        tiempoNano = self.contar_con_nano_time(tarea)
+        print(f'Tiempo Millis {tiempoMillis}')
+        print(f'Tiempo Nano {tiempoNano}')
+        #tarea () ->
+
+    def build_arreglo(self, size):
+        array =[]
+        for i in range(size):
+            numero= random.randint(0,50000)
+            array.append(numero)
+        return array
+    
+    def contar_con_current_time_milles(self,tarea):
+        inicio = time.time()
+        tarea()
+        fin = time.time()
+        return (fin - inicio)*1000
+    
+    def contar_con_nano_time(self,tarea):
+        inicio = time.perf_counter_ns()
+        tarea()
+        fin = time.perf_counter_ns()
+        return (fin - inicio) / 1_000_000_000.0
